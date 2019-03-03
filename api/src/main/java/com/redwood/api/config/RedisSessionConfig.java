416204d5b23197ae70e77f419717d72679fe1185
laymat <laymat@163.com>
2019-03-03 13:46:11 +0800

    update project setting

--- api/src/main/java/com/redwood/api/config/RedisSessionConfig.java
+++ api/src/main/java/com/redwood/api/config/RedisSessionConfig.java
@@ -1,22 +1,12 @@
 package com.redwood.api.config;
 
-import cn.hutool.core.codec.Base64Decoder;
-import cn.hutool.extra.servlet.ServletUtil;
-import cn.hutool.json.JSONUtil;
-import javafx.beans.binding.ObjectBinding;
-import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import javax.annotation.Resource;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Base64;
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 60)
